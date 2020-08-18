cf01fb160cdd80bb1047c4225d0f2841ef13e366
caudlejohnp <caudlejohnp@gmail.com>
2020-08-17 19:30:21 -0500

    added interface for product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { IProduct } from './Model/iproduct';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
 }
