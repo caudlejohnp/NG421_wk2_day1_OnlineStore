f35d69f59020837a4439197394e67b3ba4fb3fdc
caudlejohnp <caudlejohnp@gmail.com>
2020-08-18 11:05:29 -0500

    completed header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { IProduct } from "../Model/iproduct";
+import { ProductService } from "../product.service";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"],
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  shoppingCart: IProduct[] = [];
+
+  constructor(private productService: ProductService) {}
 
   ngOnInit() {
+    this.shoppingCart = [];
+    this.shoppingCart = this.productService.getShoppingCart();
   }
-
 }
